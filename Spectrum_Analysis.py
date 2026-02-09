f2276a768a381052e35186f7b83086c4d1e2a90b
Lewis Reid <56838932+lrreid@users.noreply.github.com>
2026-02-09 16:09:52 +0000

    Spectrum plotter updated following testing. Utilities folder changed name to utils.

--- Spectrum_Analysis.py
+++ Spectrum_Analysis.py
@@ -4,12 +4,16 @@ Functions to analyse laser spectrum data
 None of these have been tested.
 """
 
+import sys
 import numpy as np
 from os.path import splitext
 from os import listdir
+import matplotlib.pyplot as plt
+
 from LaserAnalysisTools.core.rounding import find_nearest
 from LaserAnalysisTools.core.statistics import w_std
 from LaserAnalysisTools.utils.background_removal import remove_spectrometer_background
+from LaserAnalysisTools.core.plot_limits import plt_limits_log, plt_limits
 
 def select_wavelength_range(wavelength_data, intensity_data, wavelength_min, wavelength_max):
     # Need to add some error handling. What if selected range is outwith the data in the file?
@@ -62,14 +66,7 @@ def analyse_spectrum(wavelength_data, intensity_data, int_threshold = 1/np.exp(1
 
     return [mean_wavelength, stat_bandwidth], [central_wavelength_e2, theshold_bandwidth, e2_blue, e2_red], wavelength_peak
 
-
-# This is partially pseudo-code, it needs fixed.
-def do_spectrum_analysis(txt_dir, file_number, remove_background: bool = False, bg_limits = [925, 950], select_data: bool = False, select_limits = [650, 950], int_threshold = 1/np.exp(1)**2):
-    # I don't think I like this method for looping through lots of data files. This function will re-find a list of all the text files in a folder numerous times. 
-    # I would also have to find text_files to see the number of text files in a given folder to see how many times to loop. Think about stacking functions in a different way?
-
-    text_files            = [splitext(f)[0] for f in listdir(txt_dir) if f.endswith('.txt')]        # List all files is folder that end in .txt
-    wavelength, intensity = np.loadtxt((open("%s\\%s.txt" % (txt_dir, text_files[file_number]),'rt').readlines()[:-1]), skiprows=0, unpack=True)
+def do_spectrum_analysis(wavelength, intensity, remove_background: bool = False, bg_limits = [925, 950], select_data: bool = False, select_limits = [650, 950], int_threshold = 1/np.exp(1)**2):
 
     if remove_background is True:
         _, intensity = remove_spectrometer_background(wavelength, intensity, bg_limits[0], bg_limits[1])
@@ -80,3 +77,71 @@ def do_spectrum_analysis(txt_dir, file_number, remove_background: bool = False, 
     [mean_wavelength, stat_bandwidth], [central_wavelength_e2, theshold_bandwidth, e2_blue, e2_red], wavelength_peak = analyse_spectrum(wavelength_data = wavelen_select, intensity_data = intensity_select, int_threshold = int_threshold)
 
     return [wavelen_select, intensity_select], [mean_wavelength, stat_bandwidth], [central_wavelength_e2, theshold_bandwidth, e2_blue, e2_red], wavelength_peak
+
+
+def plot_spectrum(txt_dir: str, remove_background: bool = False, bg_limits = [925, 950], select_data: bool = False, select_limits = [650, 950], int_threshold = 1/np.exp(1)**2, Loop_files: bool = False, file_no=0, Save_Plots: bool = True, DPI = 150, fsize = 12):
+    """
+    Add saving of summary text file
+    """
+    if Save_Plots is True:
+        from matplotlib import use
+        use("Agg")                  # "Agg" makes it possible to save plots without a display attached. Useful for analysis on remote computing cluster or saving plots in a loop without them opening.
+
+    text_files = [splitext(f)[0] for f in listdir(txt_dir) if f.endswith('.txt')]        # List all files is folder that end in .txt
+
+    if Loop_files is True:
+        Q = len(text_files)
+    else:
+        Q = len([0])
+
+    for i in range(Q):
+
+        if Loop_files is False:
+            i = file_no    
+
+        wavelength, intensity = np.loadtxt((open("%s\\%s.txt" % (txt_dir, text_files[i]),'rt').readlines()[:-1]), skiprows=0, unpack=True)
+
+        data_select, stat, bandwidth, wavelength_peak = do_spectrum_analysis(wavelength, intensity, remove_background=remove_background, bg_limits=bg_limits, select_data=select_data, select_limits=select_limits, int_threshold=int_threshold)
+
+        _, Int_max = plt_limits_log(data_select[1])      # Find max intenisty for plot limit
+        Int_min = 0
+        plt_min, plt_max = plt_limits(data_select[0], 10)
+
+        fig, ax = plt.subplots()
+        plt.plot([bandwidth[2], bandwidth[2]], [0, Int_max], '--', linewidth = 2, color="seagreen")
+        plt.plot([bandwidth[3], bandwidth[3]], [0, Int_max], '--', linewidth = 2, color="seagreen")
+        plt.plot([bandwidth[0], bandwidth[0]],   [0, Int_max], '-', linewidth = 2, color="seagreen", label="centre of 1/e$^{2}$\nboundaries:\n%0.2f nm" % (np.round(bandwidth[0],2)))
+        plt.plot([wavelength_peak, wavelength_peak], [0, Int_max], '-', linewidth = 2, color="dodgerblue", label="$\\lambda$ at peak intensity:\n%0.2f nm" % (np.round(wavelength_peak,2)))
+        plt.plot([stat[0], stat[0]], [0, Int_max], '-', linewidth = 2, color="magenta", label="weighted average:\n%0.2f nm" % (np.round(stat[0],2)))
+        plt.plot(data_select[0], data_select[1], color='firebrick')
+        plt.text((0.86*(plt_max-plt_min))+plt_min, (0.62*(Int_max-Int_min))+Int_min, "Width to 1/e$^{2}$\n%0.2f nm" % (bandwidth[1]) ,  color='seagreen', fontweight='normal', fontsize=12, horizontalalignment ="center" )
+        ax.set_title('%s' % (text_files[i]), fontsize=fsize)
+        
+        plt.axis([plt_min, plt_max, Int_min, Int_max])         # set axes [xmin, xmax, ymin, ymax]
+        ax.set_xlabel('Wavelength (nm)', fontsize=fsize)
+        ax.set_ylabel('Intensity (a.u.)', fontsize=fsize)
+        ax.ticklabel_format(axis='y',style='sci',scilimits=(0,0)) # put yticks in scientific notation
+        plt.grid(True)
+        plt.legend(loc='upper left', prop={'size': 9})
+        plt.tight_layout()
+        
+        if Save_Plots is True:
+            plt.savefig(("%s\\%s_Analysed.png" % (txt_dir, text_files[i]) ), dpi=DPI, format="png")
+            plt.close()
+
+            # Save text file with summary of analysis
+            fp =  open("%s/%s_Analysis_Summary.txt" % (txt_dir, text_files[i]),"w")     # create text file, 'w' for write, 'a' for append.
+            fp.write("Script used for analysis:\t%s" % sys.argv[0])
+            fp.write("\nFrom statistical analysis of data\n")
+            fp.write("Central wavelength:\t%0.2f nm\n" % np.round(stat[0], 2)  )
+            fp.write("Bandwidth:\t%0.2f nm\n" % np.round(stat[1], 2)  )
+            fp.write("\nFrom intensity threshold analysis method\n")
+            fp.write("Intensity threshold at %0.3f of peak intensity\n" % np.round(int_threshold, 3)  )
+            fp.write("Central wavelength:\t%0.2f nm\n" % np.round(bandwidth[0], 2)  )
+            fp.write("Bandwidth:\t%0.2f nm\n" % np.round(bandwidth[1], 2)  )
+            fp.write("\nWavelength at peak intensity:\t%0.2f nm" % np.round(wavelength_peak, 2) )
+            fp.close()
+        else:
+            plt.show()
+
+    return None
